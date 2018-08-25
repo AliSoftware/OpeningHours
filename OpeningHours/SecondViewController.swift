3da87ac4eb7ac5b7f8d2d5de6eb0bfd45e887c9e
AliSoftware <olivier@halligon.net>
2018-08-25 17:07:34 +0200

    Initial Commit

---
+++ OpeningHours/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  OpeningHours
+//
+//  Created by Olivier HALLIGON on 25/08/2018.
+//  Copyright © 2018 AliSoftware. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
