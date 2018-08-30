bba8c228dba925292e31d15c0302b14da05eb65c
AliSoftware <olivier@halligon.net>
2018-08-30 03:34:18 +0200

    Cleanup dummy values

--- OpeningHours/AppDelegate.swift
+++ OpeningHours/AppDelegate.swift
@@ -19,10 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Override point for customization after application launch.
 
     Prefs.main.registerDefaults()
-
-//    fillDummyValues()
-    ///
-
     shopsListVC.shops = Prefs.main.shops
 
     let window = UIWindow()
@@ -37,53 +33,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationDidEnterBackground(_ application: UIApplication) {
     Prefs.main.shops = shopsListVC.shops
   }
-}
-
-// TODO: Remove dummy values below
-
-extension Time: ExpressibleByFloatLiteral {
-  init(floatLiteral value: Double) {
-    self.hour = Int(value)
-    self.minutes = Int(value*100) % 100
-    assert(minutes < 60)
-  }
-}
-
-extension Time {
-  static func - (start: Time, end: Time) -> TimeRange {
-    return TimeRange(start: start, end: end)
-  }
-}
-
-func fillDummyValues() {
-  let ranges1: [TimeRange] = [
-     9.30 - 12.00,
-    13.30 - 15.00,
-    18.00 - 20.00
-  ]
-  let ranges2: [TimeRange] = [
-     9.30 - 12.00,
-    14.30 - 18.00
-  ]
-  let ranges3: [TimeRange] = [
-    10.00 - 12.00
-  ]
-  let ranges4: [TimeRange] = [
-     0.00 - 12.00,
-    14.00 - 20.00
-  ]
-  let ranges5: [TimeRange] = [
-     0.00 -  6.40
-  ]
-  let shop1 = Shop(name: "Boulangerie", timeTable: [
-    .monday: ranges1, .tuesday: ranges1, .wednesday: ranges2, .thursday: ranges1, .friday: ranges1, .saturday: ranges3
-    ])
-  let shop2 = Shop(name: "Poissonnerie Jahier", timeTable: [
-    .tuesday: ranges2, .wednesday: ranges2, .thursday: ranges1, .friday: ranges1, .saturday: ranges3, .sunday: ranges4
-    ])
-  let shop3 = Shop(name: "Jardiland", timeTable: [
-    .tuesday: ranges1, .wednesday: ranges2, .thursday: ranges1, .friday: ranges1, .saturday: ranges3, .sunday: ranges5
-    ])
-
-  Prefs.main.shops = [shop1, shop2, shop3]
 }
